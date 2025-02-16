359ceed2de8cc701177fbf167ad7c9f4f8e1c6bb
njabulotevin <ntevin25@gmail.com>
2025-02-16 15:54:31 +0200

    fixing port scanning loop

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def index():
 
 if __name__ == "__main__":
     # with app.app_context():
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT"))
+    port = int(os.environ.get("PORT", 8080)) 
+    app.run(debug=True, host="0.0.0.0", port=port)
